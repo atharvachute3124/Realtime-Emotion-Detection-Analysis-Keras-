7c6dd499429b1e2673778ae51f30d46f3a40479c
atharvachute3124 <35529379+atharvachute3124@users.noreply.github.com>
2020-03-28 18:18:04 +0530

    Add files via upload

---
+++ imageTester.py
@@ -0,0 +1,41 @@
+import os
+import cv2 as cv
+#print(cv.__file__)
+import numpy as np
+from keras.models import model_from_json
+from keras.preprocessing import image
+
+#load model
+model = model_from_json(open("fer.json", "r").read())
+#load weights
+model.load_weights('fer.h5')
+
+
+face_haar_cascade = cv.CascadeClassifier('D:\ProgramData\lib\site-packages\cv2\data\haarcascade_frontalface_default.xml')
+
+
+test_img = cv.imread('3.jpg')
+gray_img = cv.cvtColor(test_img, cv.COLOR_BGR2GRAY)
+gray_img = np.array(gray_img, dtype='uint8')
+faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.3, 5)
+
+
+for (x,y,w,h) in faces_detected:
+    cv.rectangle(test_img,(x,y),(x+w,y+h),(255,0,0),thickness=5)
+    roi_gray=gray_img[y:y+w,x:x+h]#cropping region of interest i.e. face area from  image
+    roi_gray=cv.resize(roi_gray,(48,48))
+    img_pixels = image.img_to_array(roi_gray)
+    img_pixels = np.expand_dims(img_pixels, axis = 0)
+    img_pixels /= 255
+
+    predictions = model.predict(img_pixels)
+
+    #find max indexed array
+    max_index = np.argmax(predictions[0])
+
+    emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
+    predicted_emotion = emotions[max_index]
+
+    cv.putText(test_img, predicted_emotion, (int(x), int(y)), cv.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+
+cv.imshow('img',test_img)
